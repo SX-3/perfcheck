f452be21cbcdf67e1b29452e33c2b4e64f01f432
SX3 <i4s@ro.ru>
2026-05-27 20:01:10 +0300

    fix: router

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { handleHotUpdate, routes } from 'vue-router/auto-routes'
-import App from './App.vue'
-import '@unocss/reset/tailwind-v4.css'
-import 'virtual:uno.css'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import { handleHotUpdate, routes } from 'vue-router/auto-routes';
+import App from './App.vue';
+import '@unocss/reset/tailwind-v4.css';
+import 'virtual:uno.css';
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
 if (import.meta.hot)
-  handleHotUpdate(router)
+  handleHotUpdate(router);
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
